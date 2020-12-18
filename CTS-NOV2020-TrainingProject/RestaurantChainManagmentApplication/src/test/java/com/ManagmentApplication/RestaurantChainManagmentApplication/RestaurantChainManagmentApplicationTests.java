1691182b2307b6ac3b905ec077b53c7ca14534ca
Kartik Sharma <kartik.sharma4@cognizant.com>
2020-12-18 17:53:58 +0530

    Restaurant Chain Management Application

---
+++ CTS-NOV2020-TrainingProject/RestaurantChainManagmentApplication/src/test/java/com/ManagmentApplication/RestaurantChainManagmentApplication/RestaurantChainManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ManagmentApplication.RestaurantChainManagmentApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantChainManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
